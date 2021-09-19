de605eceaada4a7a770fd2a23a915c391854ad8f
YC <f14041054@gs.ncku.edu.tw>
2021-09-19 15:49:05 +0800

    local server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const https = require('https')
+const fs = require('fs')
+const app = express()
+
+const SSL_PORT = 443
+const IP = '192.168.0.111'
+
+const key = fs.readFileSync('./cert/key.pem')
+const cert = fs.readFileSync('./cert/cert.pem')
+var credentials = {
+  key: key,
+  cert: cert
+}
+https.createServer(credentials, app).listen(SSL_PORT, IP, function() {
+  console.log(`https port:${SSL_PORT}`);
+})
+
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!')
+  res.sendFile(`${__dirname}/index.html`)
+})
