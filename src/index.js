940ddfa5e253fe4e7fc10d0c9d1030c44928350a
YC <f14041054@gs.ncku.edu.tw>
2021-09-20 13:07:21 +0800

    question

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 function toggle(element) {
   let scene = document.querySelector("#scene");
+  let exam = document.querySelector("#exam");
   if (element.innerHTML === "題目") {
     element.innerHTML = "圖片";
     scene.object3D.visible = true;
+    exam.style.display = "none";
   }
   else {
     element.innerHTML = "題目";
     scene.object3D.visible = false;
+    exam.style.display = "";
   }
 }
